bd6fed69a25486fd306727d474d0da1c6cd89c4c
bdavs <rdavis2@umassd.edu>
2016-04-15 00:08:39 -0400

    show me your genitals

--- ROM/ROM_xmdf.tcl
+++ ROM/ROM_xmdf.tcl
@@ -207,6 +207,10 @@ utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ROM.asy
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type asy
 incr fcount
 
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ROM.mif
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path ROM.ngc
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
 incr fcount
