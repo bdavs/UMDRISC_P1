eca16678e5252f6a041f493b0b655145cce2772c
Daniel Garcia <dgarcia1@umassd.edu>
2016-04-17 14:52:12 -0400

    Debug Unit stuff

---
+++ Debug/TheDebugUnit.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:48:59 04/17/2016 
+-- Design Name: 
+-- Module Name:    TheDebugUnit - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TheDebugUnit is
+end TheDebugUnit;
+
+architecture Behavioral of TheDebugUnit is
+
+begin
+
+
+end Behavioral;
+
